e21f808a802fe84953a08c8b91d97adeb5672492
DavidMinter38 <55805744+DavidMinter38@users.noreply.github.com>
2020-02-05 15:17:01 +0000

    136. Make projectiles move

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]
+    float projectileSpeed = 1f;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * projectileSpeed * Time.deltaTime);
+    }
+}
