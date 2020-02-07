4516fb3f4a477beec84f5fd2acd3a430154c5a06
DavidMinter38 <55805744+DavidMinter38@users.noreply.github.com>
2020-02-07 20:00:55 +0000

    139. Tidy up and death FX

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,8 +7,22 @@ public class Projectile : MonoBehaviour
     [SerializeField]
     float projectileSpeed = 1f;
 
+    [SerializeField]
+    float damage = 50f;
+
     void Update()
     {
         transform.Translate(Vector2.right * projectileSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var health = collision.GetComponent<Health>();
+        var attacker = collision.GetComponent<Attacker>();
+        if(attacker && health)
+        {
+            health.InflictDamage(damage);
+            Destroy(gameObject);
+        }
     }
 }
