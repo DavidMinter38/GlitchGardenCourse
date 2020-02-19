34a151c9825ed0158af20c54c2234ac2ef733465
DavidMinter38 <55805744+DavidMinter38@users.noreply.github.com>
2020-02-19 19:24:02 +0000

    158. Attackers striking target

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -7,14 +7,41 @@ public class Attacker : MonoBehaviour
     [Range (0f, 5f)]
     float currentSpeed = 1f;
 
+    GameObject currentTarget;
+
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+        if(!currentTarget)
+        {
+            GetComponent<Animator>().SetBool("IsAttacking", false);
+        }
     }
 
     public void SetMovementSpeed(float speed)
     {
         currentSpeed = speed;
+    }
+
+    public void Attack(GameObject target)
+    {
+        GetComponent<Animator>().SetBool("IsAttacking", true);
+        currentTarget = target;
+    }
+
+    public void StrikeCurrentAttacker(float damage)
+    {
+        if(!currentTarget) { return; }
+        Health health = currentTarget.GetComponent<Health>();
+        if(health)
+        {
+            health.InflictDamage(damage);
+        }
     }
 }
